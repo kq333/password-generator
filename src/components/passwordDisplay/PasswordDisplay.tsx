0f1d68639d1a36da2d48d9d02f3d81e8163403a6
kq333 <afugowski@yandex.com>
2023-10-13 18:59:44 +0200

    created funcion to handle classname and settimeout to change icon color

--- src/components/passwordDisplay/PasswordDisplay.tsx
+++ src/components/passwordDisplay/PasswordDisplay.tsx
@@ -1,11 +1,26 @@
 import './PasswordDisplay.scss';
 import copyIcon from '../../assets/copy.svg';
+import { useState, useEffect } from 'react';
 
 interface Props {
   passwordGenerated: string;
 }
 
 export const PasswordDisplay: React.FC<Props> = ({ passwordGenerated }) => {
+  const [isCoped, setIsCoped] = useState(false);
+
+  const addClassToIcon = isCoped && passwordGenerated.length > 0
+    ? 'password__icon--isActive'
+    : 'password__icon';
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isCoped) {
+        setIsCoped(false);
+      }
+    }, 2000);
+  }, [isCoped]);
+
   const handlerCopyPassword = () => {
     const tempInput = document.createElement('input');
     tempInput.value = passwordGenerated;
@@ -17,6 +32,7 @@ export const PasswordDisplay: React.FC<Props> = ({ passwordGenerated }) => {
 
     document.execCommand('copy');
 
+    setIsCoped(!isCoped);
     document.body.removeChild(tempInput);
   };
 
@@ -25,7 +41,7 @@ export const PasswordDisplay: React.FC<Props> = ({ passwordGenerated }) => {
       <div className='password__wrapper'>
         <div className='password__content'>{passwordGenerated}</div>
         <img
-          className='password__icon'
+          className={addClassToIcon}
           src={copyIcon}
           alt='copy icon'
           onClick={handlerCopyPassword}
