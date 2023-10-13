13d2125e9161759d9d9a2d1e0cb42f01784f2649
kq333 <afugowski@yandex.com>
2023-10-13 13:14:29 +0200

    added semantic to html

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import { FormComponent } from './components/form';
 import './App.scss';
 
 function App() {
-  return <FormComponent />;
+  return (
+    <main>
+      <section>
+        <FormComponent />
+      </section>
+    </main>
+  );
 }
 
 export default App;
